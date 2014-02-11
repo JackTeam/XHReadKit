443486f679333adf8b162994e99995f9e4a701d5
xhzengAIB <xhzengAIB@gmail.com>
2014-02-11 17:28:54 +0000

    Firstly init project

---
+++ XHReadKit/XHReadKitTests/XHReadKitTests.m
@@ -0,0 +1,34 @@
+//
+//  XHReadKitTests.m
+//  XHReadKitTests
+//
+//  Created by 曾 宪华 on 14-2-11.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XHReadKitTests : XCTestCase
+
+@end
+
+@implementation XHReadKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
