f17ac14953d4b0bca62f68f9f2858ba98dd777ce
xhzengAIB <xhzengAIB@gmail.com>
2014-02-12 11:32:18 +0000

    Add XHColumnManagerViewController class

---
+++ Source/XHColumnManagerViewController.m
@@ -0,0 +1,38 @@
+//
+//  XHColumnManagerViewController.m
+//  XHReadKit
+//
+//  Created by 曾 宪华 on 14-2-12.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import "XHColumnManagerViewController.h"
+
+@interface XHColumnManagerViewController ()
+
+@end
+
+@implementation XHColumnManagerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
